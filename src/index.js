bbdf919e6a5fcbc64489bbabd8c4e93bfcf106fb
babaroga <mama@mia.com>
2020-09-22 09:45:19 -0400

    setup tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './tailwind.output.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
