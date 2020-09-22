c1d10d90e55f374b96eef473949cf88e90f353fe
babaroga <mama@mia.com>
2020-09-22 13:20:53 -0400

    added bootrap material

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,13 +12,17 @@ function NavBar(props) {
 
   return (
     <React.Fragment>
-      <nav className="border-b flex justify-center">
-        <ul>
-          <li>
-            <Link to={tracks}>Tracks</Link>
+      <nav className="navbar justify-content-center">
+        <ul className="nav w-100">
+          <li className="nav-item border-bottom w-100 text-center">
+            <Link className="nav-link w-100" to={tracks}>
+              Tracks
+            </Link>
           </li>
-          <li>
-            <Link to={artists}>Artists</Link>
+          <li className="nav-item w-100 text-center">
+            <Link className="nav-link w-100" to={artists}>
+              Artists
+            </Link>
           </li>
         </ul>
       </nav>
