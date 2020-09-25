e4ff2bba8d2678a5866312e1db4e0807d7c78b6b
babaroga <mama@mia.com>
2020-09-25 01:19:15 -0400

    Updates

---
+++ precache-manifest.7053aa57608dd320a94b2ce323d2616a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07ae81d36decbc120e39281d4e4ad60",
+    "url": "/top-tracks/index.html"
+  },
+  {
+    "revision": "77027957cd34fc0a6d97",
+    "url": "/top-tracks/static/css/main.c3402c40.chunk.css"
+  },
+  {
+    "revision": "e4ce8701d829d8f124ba",
+    "url": "/top-tracks/static/js/2.de338083.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/top-tracks/static/js/2.de338083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77027957cd34fc0a6d97",
+    "url": "/top-tracks/static/js/main.420c0817.chunk.js"
+  },
+  {
+    "revision": "13674f958b46a3792d4e",
+    "url": "/top-tracks/static/js/runtime-main.71ac6bb1.js"
+  }
+]);
